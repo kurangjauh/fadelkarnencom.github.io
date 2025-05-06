b983ea6f190dd319df7ca3387db93c7511c4d5c3
Thoriq Pratamaa <helloikha@gmail.com>
2025-05-07 03:16:19 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Router from './RouterConfig';
+
+//Import fonts below
+import "../src/fonts/AeonikTRIAL-Bold.otf";
+import "../src/fonts/AeonikTRIAL-BoldItalic.otf";
+import "../src/fonts/AeonikTRIAL-Light.otf";
+import "../src/fonts/AeonikTRIAL-LightItalic.otf";
+import "../src/fonts/AeonikTRIAL-Regular.otf";
+import "../src/fonts/AeonikTRIAL-RegularItalic.otf";
+
+const App = () => {
+  return(
+    <>
+      <Router/>
+    </>
+  )
+}
+
+export default App;
